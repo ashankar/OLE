88db1df92706dd8aee494f522157099c7a9a09cb
Rakesh Singh <rakesh@ekisaan.com>
2013-01-14 14:52:54 +0530

    @ Rakesh  added for testing purpose.

--- Codebase/ole/src/com/ansy/oss/ole/client/Ole.java
+++ Codebase/ole/src/com/ansy/oss/ole/client/Ole.java
@@ -1,10 +1,18 @@
 package com.ansy.oss.ole.client;
 
-import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.sencha.gxt.widget.core.client.button.TextButton;
+import javax.swing.table.TableColumn;
 
-public class Ole implements EntryPoint
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.sencha.gxt.widget.core.client.FramedPanel;
+import com.sencha.gxt.widget.core.client.button.TextButton;
+import com.sencha.gxt.widget.core.client.container.VerticalLayoutContainer;
+import com.sencha.gxt.widget.core.client.form.FieldLabel;
+import com.sencha.gxt.widget.core.client.form.TextField;
+import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
+
+public class Ole implements EntryPoint 
 {
 	public void onModuleLoad()
 	{
---
+++ Codebase/ole/src/com/ansy/oss/ole/client/report/ViewReport.java
@@ -0,0 +1,14 @@
+package com.ansy.oss.ole.client.report;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class ViewReport implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
